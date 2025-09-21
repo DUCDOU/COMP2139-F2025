514e05539ce078d56cabf693b8c47a1fb0af4aca
DUC DOU <gianghbh192@gmail.com>
2025-09-21 14:27:11 -0400

    Lab2 Completed

--- COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
+++ COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd39c2228722d6bd14fe2432c876671eb09c0ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05335f6275547bf65ccf66dc52abe9c4c9cf0987")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
